abce006bf8fc9ef75406438006c584e253aec2c2
Joe Langford <dev@joelangford.com>
2019-06-23 14:32:29 +0100

    Check calling function to write to display

--- inkysky.py
+++ inkysky.py
@@ -1,20 +1,3 @@
-from inky import InkyPHAT
-from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
+from write import write
 
-inky_display = InkyPHAT("yellow")
-inky_display.set_border(inky_display.WHITE)
-
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-draw = ImageDraw.Draw(img)
-
-font = ImageFont.truetype(FredokaOne, 22)
-
-message = "Inky Sky"
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 2) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (h / 2)
-
-draw.text((x, y), message, inky_display.YELLOW, font)
-inky_display.set_image(img)
-inky_display.show()
+write('Test')

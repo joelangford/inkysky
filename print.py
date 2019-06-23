074ef47747a4805b0718a4994f7c04cd55f4ce42
Joe Langford <dev@joelangford.com>
2019-06-23 15:21:00 +0100

    changed code to do full refresh

--- print.py
+++ print.py
@@ -5,10 +5,10 @@ from font_fredoka_one import FredokaOne
 inky_display = InkyPHAT("yellow")
 inky_display.set_border(inky_display.WHITE)
 
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-draw = ImageDraw.Draw(img)
-
 def printToInky(temperature, summary):
+    img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+    draw = ImageDraw.Draw(img)
+
     temperatureFont = ImageFont.truetype(FredokaOne, 22)
     draw.text((10, 10), temperature, inky_display.BLACK, temperatureFont)
 

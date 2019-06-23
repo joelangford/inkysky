20a593e9c67e6580484e4496bf41711bbdbea72c
Joe Langford <dev@joelangford.com>
2019-06-23 15:23:49 +0100

    Inverse colours

--- print.py
+++ print.py
@@ -3,17 +3,17 @@ from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 
 inky_display = InkyPHAT("yellow")
-inky_display.set_border(inky_display.WHITE)
+inky_display.set_border(inky_display.BLACK)
 
 def printToInky(temperature, summary):
     img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
     draw = ImageDraw.Draw(img)
 
     temperatureFont = ImageFont.truetype(FredokaOne, 22)
-    draw.text((10, 10), temperature, inky_display.BLACK, temperatureFont)
+    draw.text((10, 10), temperature, inky_display.WHITE, temperatureFont)
 
     summaryFont = ImageFont.truetype(FredokaOne, 12)
-    draw.text((10, 40), summary, inky_display.BLACK, summaryFont)
+    draw.text((10, 40), summary, inky_display.WHITE, summaryFont)
 
     inky_display.set_image(img)
     inky_display.show()

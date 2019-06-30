a20a8212f643b7d4208303e1e0160a4a091b5ff2
Joe Langford <dev@joelangford.com>
2019-06-30 11:19:08 +0100

    Changed indentation

--- print.py
+++ print.py
@@ -8,10 +8,13 @@ inky_display.set_border(inky_display.BLACK)
 
 def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
     """Create a transparency mask.
+
     Takes a paletized source image and converts it into a mask
     permitting all the colours supported by Inky pHAT (0, 1, 2)
     or an optional list of allowed colours.
+
     :param mask: Optional list of Inky pHAT colours to allow.
+
     """
     mask_image = Image.new("1", source.size)
     w, h = source.size
@@ -20,7 +23,8 @@ def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_displ
             p = source.getpixel((x, y))
             if p in mask:
                 mask_image.putpixel((x, y), 255)
-                return mask_image
+
+    return mask_image
 
 icons = {}
 masks = {}

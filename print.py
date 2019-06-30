b879d2eb79349368f42073b19fdec56c8458ed9e
Joe Langford <dev@joelangford.com>
2019-06-30 10:10:02 +0100

    corrected syntax

--- print.py
+++ print.py
@@ -58,8 +58,8 @@ def printToInky(temperature, summary, iconType):
             weather_icon = icon
             break
 
-    print(print ', '.join(icons))
-    print(print ', '.join(masks))
+    print ', '.join(icons)
+    print ', '.join(masks)
     print(weather_icon)
 
     iconXposition = inky_display.WIDTH - 28
